a944790df6b4c079bc37d998c9d3c7176b4fe275
Sean Barry <se@nbarry.co.uk>
2021-12-07 21:23:28 +0000

    17.1

--- compiler.c
+++ compiler.c
@@ -3,21 +3,12 @@
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "vm.h"
 
-void compile(const char* source) {
+void compile(const char *source, Chunk *chunk)
+{
     initScanner(source);
-
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("#4d ", token.line);
-            line = token.line;
-        } else {
-            printf ("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        if (token.type == TOKEN_EOF) break;
-    }
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
